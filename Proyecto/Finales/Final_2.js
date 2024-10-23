ffaaea03f50ae1709de1ea2301f649c9498bd12a
TobiEtbul <etbultobias@gmail.com>
2024-10-23 10:46:51 -0300

    Suman los cuadrados

--- Proyecto/Finales/Final_2.js
+++ Proyecto/Finales/Final_2.js
@@ -1,9 +1,11 @@
 const squares = document.querySelectorAll('.barra_vida .square');
-let currentSquare = 0; // Controla cuál cuadrado se está encendiendo
+let currentSquare = 0;
 
-document.querySelector('#toggleButton').addEventListener('click', function() {
+let algo = document.querySelector('#toggleButton');
+
+algo.addEventListener('click', function() {
     if (currentSquare < squares.length) {
-        squares[currentSquare].classList.add('active'); // Enciende el cuadrado actual
-        currentSquare++; // Pasa al siguiente cuadrado
+        squares[currentSquare].classList.add('active'); 
+        currentSquare++; 
     }
 });
