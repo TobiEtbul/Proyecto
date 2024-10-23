9f2e574903f6f3283ac2315855aefa9b903e2513
TobiEtbul <etbultobias@gmail.com>
2024-10-23 10:44:05 -0300

    Delete Proyecto/Finales directory

--- Proyecto/Finales/Final_2.js
+++
@@ -1,9 +0,0 @@
-const squares = document.querySelectorAll('.barra_vida .square');
-let currentSquare = 0; // Controla cuál cuadrado se está encendiendo
-
-document.querySelector('#toggleButton').addEventListener('click', function() {
-    if (currentSquare < squares.length) {
-        squares[currentSquare].classList.add('active'); // Enciende el cuadrado actual
-        currentSquare++; // Pasa al siguiente cuadrado
-    }
-});
