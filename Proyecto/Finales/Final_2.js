ce615d898a259d04bab17e7d0f0408fdc6cdb352
TobiEtbul <etbultobias@gmail.com>
2024-10-28 09:50:46 -0300

    Todas las barras de cristales funcionan

--- Proyecto/Finales/Final_2.js
+++ Proyecto/Finales/Final_2.js
@@ -1,7 +1,7 @@
-const squaresV = document.querySelectorAll('.barra_vida .square');
+const squaresV = document.querySelectorAll('.barra_vida .squareV');
 let currentSquareV = 0;
 
-let algoV = document.querySelector('#toggleButton');
+let algoV = document.querySelector('#toggleButtonV');
 
 algoV.addEventListener('click', function() {
     if (currentSquareV < squaresV.length) {
@@ -11,10 +11,10 @@ algoV.addEventListener('click', function() {
 });
 
 
-const squaresDe = document.querySelectorAll('.barra_defensa .square');
+const squaresDe = document.querySelectorAll('.barra_defensa .squareDe');
 let currentSquareDe = 0;
 
-let algoDe = document.querySelector('#toggleButton');
+let algoDe = document.querySelector('#toggleButtonDe');
 
 algoDe.addEventListener('click', function() {
     if (currentSquareDe < squaresDe.length) {
@@ -23,3 +23,38 @@ algoDe.addEventListener('click', function() {
     }
 });
 
+const squaresDa = document.querySelectorAll('.barra_daño .squareDa');
+let currentSquareDa = 0;
+
+let algoDa = document.querySelector('#toggleButtonDa');
+
+algoDa.addEventListener('click', function() {
+    if (currentSquareDa < squaresDa.length) {
+        squaresDa[currentSquareDa].classList.add('active'); 
+        currentSquareDa++; 
+    }
+});
+
+const squaresA = document.querySelectorAll('.barra_area .squareA');
+let currentSquareA = 0;
+
+let algoA = document.querySelector('#toggleButtonA');
+
+algoA.addEventListener('click', function() {
+    if (currentSquareA < squaresA.length) {
+        squaresA[currentSquareA].classList.add('active'); 
+        currentSquareA++; 
+    }
+});
+
+const squaresS = document.querySelectorAll('.barra_stamina .squareS');
+let currentSquareS = 0;
+
+let algoS = document.querySelector('#toggleButtonS');
+
+algoS.addEventListener('click', function() {
+    if (currentSquareS < squaresS.length) {
+        squaresS[currentSquareS].classList.add('active'); 
+        currentSquareS++; 
+    }
+});
