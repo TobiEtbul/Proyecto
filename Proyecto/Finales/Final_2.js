15dc67973758e2c45f7fd4caedea156d32d6aaba
TobiEtbul <etbultobias@gmail.com>
2024-10-28 11:16:42 -0300

    knj

--- Proyecto/Finales/Final_2.js
+++ Proyecto/Finales/Final_2.js
@@ -1,27 +1,44 @@
+const cristales = document.querySelector('.num_cristales')
 const squaresV = document.querySelectorAll('.barra_vida .squareV');
 let currentSquareV = 0;
 
 let algoV = document.querySelector('#toggleButtonV');
 
 algoV.addEventListener('click', function() {
+    let crystalCount = parseInt(cristales.textContent);
+    if (crystalCount > 0) {
     if (currentSquareV < squaresV.length) {
         squaresV[currentSquareV].classList.add('active'); 
         currentSquareV++; 
+
+         // Obtiene el valor actual del h2
+        // Solo resta si el número es mayor a 0
+            cristales.textContent = crystalCount - 1; // Actualiza el número en el h2
     }
+}
 });
+
+
+
 
 
 const squaresDe = document.querySelectorAll('.barra_defensa .squareDe');
 let currentSquareDe = 0;
 
 let algoDe = document.querySelector('#toggleButtonDe');
-
 algoDe.addEventListener('click', function() {
+    if (crystalCount > 0) {
     if (currentSquareDe < squaresDe.length) {
         squaresDe[currentSquareDe].classList.add('active'); 
         currentSquareDe++; 
+        crystalCountElement.textContent = crystalCount - 1;
+    }
     }
 });
+
+
+
+
 
 const squaresDa = document.querySelectorAll('.barra_daño .squareDa');
 let currentSquareDa = 0;
@@ -55,6 +72,6 @@ let algoS = document.querySelector('#toggleButtonS');
 algoS.addEventListener('click', function() {
     if (currentSquareS < squaresS.length) {
         squaresS[currentSquareS].classList.add('active'); 
-        currentSquareS++; 
+        currentSquareS++;
     }
 });
