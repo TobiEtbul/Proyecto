8305a7b73c5cbfe7bc5399577155e6b2d38bac8b
TobiEtbul <etbultobias@gmail.com>
2024-11-06 10:19:34 -0300

    soquet

--- Proyecto/Finales/Final_3_partida.js
+++ Proyecto/Finales/Final_3_partida.js
@@ -1,1 +1,5 @@
-fetchData(generarEnemigoAleatorioz1)
+console.log("loaded.");
+fetchData("pedirEnemigos", (data) => {
+    //Aca adentro existe data que tiene todos los enemigos:
+    console.log("enemigos: ", data);
+});
--- soqueticServer.js
+++ soqueticServer.js
@@ -1,23 +1,21 @@
 import { onEvent, startServer } from "soquetic";
 import {
-  generarEnemigoAleatorioZ1,
-  generarEnemigoAleatorioZ2,
-  generarEnemigoAleatorioZ3,
+    generarEnemigoAleatorioZ1,
+    generarEnemigoAleatorioZ2,
+    generarEnemigoAleatorioZ3,
 } from "./main.js";
 
-onEvent("pedirEnemigo1", () => {
-  const enemigo = generarEnemigoAleatorioZ1();
-  return enemigo;
-});
-
-onEvent("pedirEnemigo2", () => {
-  const enemigo = generarEnemigoAleatorioZ2();
-  return enemigo;
-});
-
-onEvent("pedirEnemigo3", () => {
-  const enemigo = generarEnemigoAleatorioZ3();
-  return enemigo;
+onEvent("pedirEnemigos", () => {
+    const enemigo1 = generarEnemigoAleatorioZ1();
+    const enemigo2 = generarEnemigoAleatorioZ1();
+    const enemigo3 = generarEnemigoAleatorioZ1();
+    const data = {
+        enemigo1,
+        enemigo2,
+        enemigo3,
+    };
+    console.log(data)
+    return data;
 });
 
 startServer();
