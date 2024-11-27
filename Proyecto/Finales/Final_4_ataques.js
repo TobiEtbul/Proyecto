2d0799993b6688dbff4495d3f0dd45fde729ddf4
TobiEtbul <etbultobias@gmail.com>
2024-11-27 09:09:19 -0300

    vavavav

--- Proyecto/Finales/Final_4_ataques.js
+++ Proyecto/Finales/Final_4_ataques.js
@@ -5,10 +5,6 @@ const $enemigos = [
 ];
 const enemigos = JSON.parse(localStorage.getItem("enemigos"));
 
-enemigos.enemigo1["initialVida"] = enemigos.enemigo1.HP;
-enemigos.enemigo2["initialVida"] = enemigos.enemigo2.HP;
-enemigos.enemigo3["initialVida"] = enemigos.enemigo3.HP;
-
 $enemigos[0].src =
     "http://localhost:5500/Proyecto/Imagenes/Enemigos/" +
     enemigos.enemigo1.name.toLowerCase() +
@@ -22,31 +18,28 @@ $enemigos[2].src =
     enemigos.enemigo3.name.toLowerCase() +
     ".png";
 
-function restarVida( enemigo, ataque, enemigoID) {
+function restarVida(enemigo, ataque, enemigoID) {
     console.log(enemigo);
-    
-
 }
 
 function acutalizarBarra(enemigo, barra) {
-    let porcentajeVida = enemigo.HP * 100 / enemigo.HPT;
-        barra.style.width =
-            porcentajeVida + "%";
+    let porcentajeVida = (enemigo.HP * 100) / enemigo.HPM;
+    barra.style.width = porcentajeVida + "%";
 }
-
-const $golpear = document.getElementById("golpear")
-$golpear.addEventListener("click", () => {
-    console.log("clicked")
-    fetchData("golpear", (enemigos) => {
-        console.log(enemigos)
-    })
-    acutalizarBarra($enemigos[0], barras[0])
-    acutalizarBarra($enemigos[1], barras[1])
-    acutalizarBarra($enemigos[2], barras[2])
-})
-
-barras = [
+const barras = [
     document.getElementById("vidaEnemigo1"),
     document.getElementById("vidaEnemigo2"),
-    document.getElementById("vidaEnemigo3")
-]
+    document.getElementById("vidaEnemigo3"),
+];
+const $golpear = document.getElementById("golpear");
+$golpear.addEventListener("click", () => {
+    console.log("clicked");
+    let ene;
+    fetchData("golpear", (enemigos) => {
+        ene = enemigos;
+        console.log(ene);
+        acutalizarBarra(ene[0], barras[0]);
+        acutalizarBarra(ene[1], barras[1]);
+        acutalizarBarra(ene[2], barras[2]);
+    });
+});
